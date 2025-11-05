bdf6f02772c2e67cfa09ac97f47404d59acdb370
soufyane <s.yassine@siliconesignal.com>
2025-11-05 16:53:32 +0000

    preparing files

---
+++ paramiko_automation/Readme.md
@@ -0,0 +1,2 @@
+## installation setup
+$ pip install paramiko
---
+++ paramiko_automation/ssh_automation.py
@@ -0,0 +1,16 @@
+import paramiko
+
+
+
+
+class SSHManager:
+       def __init__(self, username, password):
+            
+            pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
