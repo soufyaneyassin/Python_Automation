a3fd25e9c3f3c90d468024e487f674cb4290e841
soufyane <s.yassine@siliconesignal.com>
2025-11-06 14:29:49 +0000

    preparing SSHManager class

--- paramiko_automation/ssh_automation.py
+++ paramiko_automation/ssh_automation.py
@@ -5,9 +5,23 @@ import paramiko
 
 class SSHManager:
        def __init__(self, username, password):
-            
+            self.username = username
+            self.password = password
+            self.ssh_client = paramiko.SSHClient()
+            self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             pass
-
+       
+       def connect(self, hostname):
+            pass
+       
+       def execute_command(self, command):
+            pass
+       
+       def copy_file_to_remote(self, localfile, remotefile):
+            pass
+       
+       def copy_file_from_remote(self, remotefile, localfile):
+            pass
 
 def main():
     pass
